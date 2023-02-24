71051f799313f4bd27f2661eb1a85c39005e9c13
Emzzy241 <emzzyoluwole@gmail.com>
2023-02-24 05:25:49 +0100

    adding my C# Code file

---
+++ MyAtm.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+
+class MyAtm{
+    static void Main(){
+        Console.WriteLine("Welcome TO the ATM APP; THis App is currently being built and this is not the final Product");
+    }
+}
